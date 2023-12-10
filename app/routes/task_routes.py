13da146e22414866f2b197d7ab0442f1bc6b0e39
Adam <kazai.adam@gmail.com>
2023-12-10 20:26:00 +0100

    feat: add task service as dependency to routes

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -1,23 +1,22 @@
-from fastapi import APIRouter
-from typing import List
+from fastapi import APIRouter, Depends
 
 from ..models.api_response import ApiResponse
 from ..models.task import Task
-from ..services import task_service
+from ..services.task_service import TaskService
 
 
 router = APIRouter()
 
 @router.get("/tasks", response_model=ApiResponse, summary="Get all tasks")
-async def get_tasks():
+async def get_tasks(task_service: TaskService = Depends(TaskService.get)):
     return task_service.get_tasks()
 
 
 @router.post("/tasks", response_model=ApiResponse, summary="Create a new task")
-async def create_task(task: Task):
+async def create_task(task: Task, task_service: TaskService = Depends(TaskService.get)):
     return task_service.create_task(task)
 
 
 @router.get("/tasks/{task_id}", response_model=ApiResponse, summary="Get a task by ID")
-async def get_task(task_id: str):
+async def get_task(task_id: str, task_service: TaskService = Depends(TaskService.get)):
     return task_service.get_task(task_id)
--- app/services/task_service.py
+++ app/services/task_service.py
@@ -4,34 +4,40 @@ from ..models.api_response import ApiResponse
 from ..models.task import Task
 from ..config import dynamodb_client
 
-def get_tasks():
-    table = dynamodb_client.Table('todolist-api-dev')
-    response = table.scan()
+class TaskService:
 
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        return ApiResponse(error=True, message="Failed to retrieve tasks", status_code=500)
+    def __init__(self):
+        self.table = dynamodb_client.Table('todolist-api-dev')
+
+    def get_tasks(self):
+        response = self.table.scan()
+
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            return ApiResponse(error=True, message="Failed to retrieve tasks", status_code=500)
+        
+        return ApiResponse(data=response['Items'], status_code=200, error=False)
+
+
+    def get_task(self, task_id: str):
+        response = self.table.get_item(Key={'id': task_id})
+
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            return ApiResponse(error=True, message="Failed to retrieve task", status_code=500)
+        
+        return ApiResponse(data=response['Item'], status_code=200, error=False)
+
+
+    def create_task(self, task: Task):
+
+        task.id = str(uuid4())
+        
+        response = self.table.put_item(Item=task.model_dump())
+
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            return ApiResponse(error=True, message="Failed to create task", status_code=500)
+
+        return ApiResponse(error=False, data=task, status_code=201)
     
-    return ApiResponse(data=response['Items'], status_code=200, error=False)
-
-
-def get_task(task_id: str):
-    table = dynamodb_client.Table('todolist-api-dev')
-    response = table.get_item(Key={'id': task_id})
-
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        return ApiResponse(error=True, message="Failed to retrieve task", status_code=500)
-    
-    return ApiResponse(data=response['Item'], status_code=200, error=False)
-
-
-def create_task(task: Task):
-    table = dynamodb_client.Table('todolist-api-dev')
-
-    task.id = str(uuid4())
-    
-    response = table.put_item(Item=task.model_dump())
-
-    if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-        return ApiResponse(error=True, message="Failed to create task", status_code=500)
-
-    return ApiResponse(error=False, data=task, status_code=201)
+    @classmethod
+    def get(cls):
+        return cls()
