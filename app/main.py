b1efd570767d388d08037731a574041a4e73e8ea
Adam <kazai.adam@gmail.com>
2024-01-15 22:27:08 +0100

    fix: allow all origins

--- app/main.py
+++ app/main.py
@@ -18,8 +18,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"]
 )
